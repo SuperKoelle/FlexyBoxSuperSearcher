a60dac5b012d71fc617062386478705ddf050b51
John List Kølle <John@jk.home>
2020-10-23 21:13:33 +0200

    ExtensionMethods

---
+++ FlexyBoxSuperSearcher/ExtensionMethods.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    /// <summary>
+    /// By using extension methods the methods can be use anywhere in the code. Dorethy likes statistics so maybe she wants
+    /// same methods to be used in future iterations.
+    /// </summary>
+    public static class FlexyBoxExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static int LetterCount(this String str)
+        {
+            return str.Count(char.IsLetter);
+        }
+
+        public static int NumberCount(this String str)
+        {
+           return str.Count(char.IsNumber);
+        }
+        
+        /// <summary>
+        /// Using Microsoft definition for symbols: https://docs.microsoft.com/en-us/dotnet/api/system.char.issymbol?view=netcore-3.1
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int SymbolCount(this String str)
+        {
+           return str.Count(char.IsSymbol);
+        }
+
+        public static Dictionary<char, int> CharaterRepresentation(this String str)
+        {
+            var result = new Dictionary<char, int>();
+
+            foreach(char c in str.ToCharArray())
+            {
+                if (result.ContainsKey(c))
+                {
+                    result[c]++;
+                }
+                else
+                {
+                    result.Add(c,1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- FlexyBoxSuperSearcher/Program.cs
+++ FlexyBoxSuperSearcher/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExtensionMethods;
+using System;
 
 namespace FlexyBoxSuperSearcher
 {
@@ -6,7 +7,33 @@ namespace FlexyBoxSuperSearcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("FlexyBox SuperSearcher");
+            Console.Write("Please enter the name you want to search for and press 'enter': ");
+            //var consoleInput = Console.ReadLine();
+            var consoleInput = "Hej verden 2020 @§";
+            Console.WriteLine("You typed in: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(consoleInput);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            PrintStatistics(consoleInput);
+        }
+
+        private static void PrintStatistics(string input)
+        {
+            Console.WriteLine("Statistics for the input string:");
+            Console.WriteLine("- Length: " + input.Length);
+            Console.WriteLine("- Amount of words: " + input.WordCount());
+            Console.WriteLine("- Amount of letters: " + input.LetterCount());
+            Console.WriteLine("- Amount of numbers: " + input.NumberCount());
+            Console.WriteLine("- Amount of symbols: " + input.SymbolCount());
+            Console.WriteLine("- Following characters is represented:");
+            
+            foreach(var kv in input.CharaterRepresentation())
+            {
+                Console.WriteLine("- - " + kv.Key + " is represented " + kv.Value + " times.");
+            }
+
         }
     }
 }
